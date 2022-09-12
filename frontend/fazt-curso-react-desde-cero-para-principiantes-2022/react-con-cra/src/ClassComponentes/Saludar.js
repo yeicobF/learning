31bf773b95b59ff7bc55aebc5bef61532b5bea1e
yeicobF <jacobrdz34@gmail.com>
2022-09-12 14:33:44 -0500

    Add: ClassComponent

---
+++ frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/ClassComponentes/Saludar.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export class Saludar extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello, world desde Class Component!</h1>
+        <p>
+          Uso más que nada informativo - no recomendable utilizar, hay una mejor
+          sintaxis
+        </p>
+      </>
+    );
+  }
+}
--- frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/index.js
+++ frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Saludar } from "./ClassComponentes/Saludar";
 import { TaskCard } from "./Styles/Task";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
+    <Saludar />
     <TaskCard ready={false} />
     <TaskCard ready />
   </>,
