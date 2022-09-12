1f5861d9bc4b7c693c59d0a6643c3f374c29238e
yeicobF <jacobrdz34@gmail.com>
2022-09-12 13:20:27 -0500

    Add: Greeting con operador ternario

--- frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/index.js
+++ frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/index.js
@@ -7,17 +7,35 @@ const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 function Greeting() {
-  return <h1>Hello, world desde componente!</h1>;
+  const name = "Jake";
+  const isMarried = true;
+
+  // JSX nos permite retornar HTML sin que sean strings. También podemos tener
+  // JavaScript.
+  // return <h1>Hello, {name}!</h1>;
+
+  if (isMarried) {
+    return <h1>Estoy casado</h1>;
+  }
+
+  return <h1>No estoy casado</h1>;
+}
+
+// Lo mismo que Greeting, pero más corto haciendo uso de operadores ternarios.
+function GreetingTernaryOperator() {
+  const isMarried = true;
+
+  return <h1>{isMarried ? "Estoy casado 😀" : "No estoy casado 😅"}</h1>;
 }
 
 // render espera elementos hijos.
 root.render(
   <div>
+    {/* Self closing tags */}
+    <GreetingTernaryOperator />
     {/* Una forma para llamar al componente. */}
     {Greeting()}
     {/* Otra forma de llamar al comonente. */}
     <Greeting></Greeting>
-    {/* Self closing tags */}
-    <Greeting />
   </div>,
 );
