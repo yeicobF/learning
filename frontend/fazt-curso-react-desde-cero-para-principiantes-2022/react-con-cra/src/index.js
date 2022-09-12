08342a35d566988497e14f89a872721f410b98fc
yeicobF <jacobrdz34@gmail.com>
2022-09-12 13:12:11 -0500

    Add(fazt-react): Formas de llamar a un componente

--- frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/index.js
+++ frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/index.js
@@ -6,5 +6,18 @@ const rootElement = document.getElementById("root");
 // Devuelve toda la aplicación inicial, el elemento root (raíz).
 const root = ReactDOM.createRoot(rootElement);
 
+function Greeting() {
+  return <h1>Hello, world desde componente!</h1>;
+}
+
 // render espera elementos hijos.
-root.render(<h1>Hola, mundo!</h1>);
+root.render(
+  <div>
+    {/* Una forma para llamar al componente. */}
+    {Greeting()}
+    {/* Otra forma de llamar al comonente. */}
+    <Greeting></Greeting>
+    {/* Self closing tags */}
+    <Greeting />
+  </div>,
+);
