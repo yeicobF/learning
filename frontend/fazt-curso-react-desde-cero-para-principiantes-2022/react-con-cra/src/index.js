b877f4ded3a35041f202a97f4033033cf7cb4424
yeicobF <jacobrdz34@gmail.com>
2022-09-12 13:27:26 -0500

    fazt-react: Otras formas de utilizar componentes

--- frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/index.js
+++ frontend/fazt-curso-react-desde-cero-para-principiantes-2022/react-con-cra/src/index.js
@@ -21,6 +21,41 @@ function Greeting() {
   return <h1>No estoy casado</h1>;
 }
 
+function GreetingWithInternalFunction() {
+  function add(x, y) {
+    return x + y;
+  }
+
+  return (
+    <div>
+      <h1>{add(10, 30)}</h1>
+    </div>
+  );
+}
+
+function GreetingWithObject() {
+  const isMarried = true;
+  const user = {
+    firstName: "Jake",
+    lastName: "Doe",
+  };
+
+  // No puede haber un objeto hijo en React. No sabe cómo interpretarlo. Esto da error.
+  // return <h1>{user}</h1>;
+
+  // Esta no es una forma común de mostrar un objeto en la aplicación.
+  // return <h1>{JSON.stringify(user)}</h1>;
+
+  return (
+    <div>
+      <h1>{user.firstName}</h1>
+      <h3>{user.lastName}</h3>
+      {/* Mostrar booleano. Sin el toString no mostrará nada. */}
+      <p>{isMarried.toString()}</p>
+    </div>
+  );
+}
+
 // Lo mismo que Greeting, pero más corto haciendo uso de operadores ternarios.
 function GreetingTernaryOperator() {
   const isMarried = true;
@@ -31,6 +66,8 @@ function GreetingTernaryOperator() {
 // render espera elementos hijos.
 root.render(
   <div>
+    <GreetingWithInternalFunction />
+    <GreetingWithObject />
     {/* Self closing tags */}
     <GreetingTernaryOperator />
     {/* Una forma para llamar al componente. */}
