531233f5c54ae243c9eec351d3dddcc8b451ba88
yeicobF <jacobrdz34@gmail.com>
2022-09-19 16:58:59 -0500

    Add: Navegación programática con router.push
    Esta no es la forma ideal de navegar, pero hay casos en los que se
    podría utilizar, como cuando se autentica un usuario.

--- frontend/midudev-nextjs-12-desde-cero/news-app/pages/index.js
+++ frontend/midudev-nextjs-12-desde-cero/news-app/pages/index.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,6 +14,15 @@ export default function Home() {
       </Head>
 
       <h1>Aprendiendo Next.js desde cero</h1>
+      <Link href="/about">Ir a about</Link>
+
+      {/* 
+        No hacer esto. La navegación programática puede tener sentido en un 
+        formulario. 
+      */}
+      <button onClick={() => router.push("/article/2")}>
+        Navegar de forma programática a un artículo
+      </button>
     </div>
   );
 }
