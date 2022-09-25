30599d33867c6ce60639dde209adaea287372999
yeicobF <jacobrdz34@gmail.com>
2022-09-25 15:55:30 -0500

    Add: Mejorar estilos de navbar

--- frontend/midudev-nextjs-12-desde-cero/xkcd-app/components/Header.js
+++ frontend/midudev-nextjs-12-desde-cero/xkcd-app/components/Header.js
@@ -1,5 +1,6 @@
 import { Container, Text } from "@nextui-org/react"
-import { Link } from "@nextui-org/react"
+import { Link as NextUILink } from "@nextui-org/react"
+import Link from "next/link"
 
 export default function Header() {
   return (
@@ -10,25 +11,25 @@ export default function Header() {
       justify="space-between"
       className="gap-2"
     >
-      <span className="flex gap-2">
+      <h1 className="flex gap-2">
         <Text weight="semibold">next</Text>
         <Text weight="extrabold">xkcd</Text>
-      </span>
+      </h1>
       <nav>
         <ul className="flex flex-row gap-2 items-center">
           <li>
-            <Link href="/" underline>
-              Home
+            <Link href="/" passHref>
+              <NextUILink underline>Home</NextUILink>
             </Link>
           </li>
           <li>
-            <Link href="/about" underline>
-              About
+            <Link href="/about" passHref>
+              <NextUILink underline>About</NextUILink>
             </Link>
           </li>
           <li>
-            <Link href="/search" underline>
-              Search
+            <Link href="/search" passHref>
+              <NextUILink underline>Search</NextUILink>
             </Link>
           </li>
         </ul>
