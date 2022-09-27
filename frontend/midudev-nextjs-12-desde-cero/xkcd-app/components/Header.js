e076d3140afdf3a60c1cef6968fe007641e8e689
yeicobF <jacobrdz34@gmail.com>
2022-09-27 06:41:14 -0500

    Add: Mostrar resultados de autocomplete en dropdown y página de resultados
    - Estilos del dropdown con los resultados mejorados.
    - Ahora hay un botón hasta abajo para mostrar todos los resultados de
      búsqueda.
    
      - PROBLEMA
    
        - A pesar de que ya solo haya encontrado 3 resultados, por ejemplo,
          sigue mostrando "ver 10 resultados" y solo muestra los correctos
          en la pantalla de todos los resultados cuando le damos click a ese
          botón.
    
    - Pantalla en donde se muestran todos los resultados de una búsqueda.
      Nos manda ahí cuando le damos click a "Ver n resultados".
    
    - Instalé un plugin de Tailwind con npm que ordena los nombres de las
      clases. Es oficial y auto-detectado  por Prettier en VSCode, por lo
      que funciona sin instalar nada más que el paquete y ejecutar Prettier
      en el editor.

--- frontend/midudev-nextjs-12-desde-cero/xkcd-app/components/Header.js
+++ frontend/midudev-nextjs-12-desde-cero/xkcd-app/components/Header.js
@@ -2,13 +2,21 @@ import { Button, Container, Text } from "@nextui-org/react"
 import { Link as NextUILink } from "@nextui-org/react"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export function Header() {
   const [results, setResults] = useState([])
+  const searchRef = useRef()
+  // Optional chaining porque value podría no existir, ser undefined.
+  const q = searchRef.current?.value
 
-  const handleChange = (e) => {
-    fetch(`/api/search?q=${e.target.value}`)
+  const handleChange = () => {
+    if (!q) {
+      setResults([])
+      return
+    }
+
+    fetch(`/api/search?q=${q}`)
       .then((res) => res.json())
       .then((searchResults) => {
         setResults(searchResults)
@@ -16,8 +24,8 @@ export function Header() {
   }
 
   return (
-    <header className="flex justify-between items-center p-4 max-w-xl m-auto">
-      <h1 className="flex gap-2 hover:opacity-80 transition">
+    <header className="m-auto flex max-w-xl items-center justify-between p-4">
+      <h1 className="flex gap-2 transition hover:opacity-80">
         <Text weight="bold">
           <Link href="/" passHref>
             <NextUILink color="secondary" underline>
@@ -27,32 +35,38 @@ export function Header() {
         </Text>
       </h1>
       <nav>
-        <ul className="flex flex-row gap-2 items-center">
-          <li>
-            <Link href="/" passHref>
-              <NextUILink underline className="font-semibold">
-                Home
-              </NextUILink>
-            </Link>
-          </li>
+        <ul className="flex flex-row items-center gap-2">
           <li className="relative">
-            <input type="search" onChange={handleChange} />
+            <input
+              type="search"
+              onChange={handleChange}
+              ref={searchRef}
+              className="rounded-3xl border border-gray-400 px-4 py-1 text-xs"
+              // className="p-1 rounded-t-sm bg-orange-100 border-b-orange-500 border-b-2"
+            />
+
             {Boolean(results.length) && (
-              <div className="absolute top-full left-0">
-                <ul className="z-50">
-                  {results.map((result) => {
-                    return (
-                      <li key={result.id}>
-                        <Link href={`/comic/${result.id}`}>
-                          <a className="text-sm font-semibold">
-                            {result.title}
-                          </a>
-                        </Link>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
+              <ul className="absolute left-0 top-full z-10 w-full overflow-hidden rounded-lg border border-gray-50 bg-white shadow-xl">
+                {results.map((result) => {
+                  return (
+                    <li key={result.id} className="m-0">
+                      <Link href={`/comic/${result.id}`}>
+                        <a className="block px-2 py-1 text-sm font-semibold hover:bg-slate-200">
+                          {result.title}
+                        </a>
+                      </Link>
+                    </li>
+                  )
+                })}
+
+                <li key="all-results" className="m-0">
+                  <Link href={`/search?q=${q}`}>
+                    <a className="block overflow-hidden text-ellipsis whitespace-nowrap px-2 py-1 text-sm font-semibold italic text-gray-400 hover:bg-slate-200">
+                      Ver {results.length} resultados
+                    </a>
+                  </Link>
+                </li>
+              </ul>
             )}
           </li>
         </ul>
