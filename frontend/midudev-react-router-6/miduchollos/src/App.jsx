77526cf3bec5c9b0ac4807b07acbf383b9034b4a
yeicobF <jacobrdz34@gmail.com>
2022-10-13 16:26:47 -0500

    Add: Rutas dinamicas y useParams

--- frontend/midudev-react-router-6/miduchollos/src/App.jsx
+++ frontend/midudev-react-router-6/miduchollos/src/App.jsx
@@ -1,13 +1,51 @@
-import { Link, Route, Routes } from "react-router-dom"
+import { Link, Route, Routes, useParams } from "react-router-dom"
 import "./App.css"
 
 // Un componente devuelve elementos. El componente es la función sin ejecutar.
 // Es lo que fabrica los elementos. Podemos tener un componente con 80 elementos.
 const Home = () => <h1>Home</h1>
 
-const SearchPage = () => <h1>Search Page</h1>
+const SearchPage = () => {
+  const tacos = [
+    "Al Pastor",
+    "Carnitas",
+    "Pastor",
+    "Suadero",
+    "Lengua",
+    "Tripa",
+    "Cabeza",
+    "Bistec",
+    "Chorizo",
+  ]
 
-function App() {
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <ul>
+        {tacos.map((taco) => (
+          <li key={taco}>
+            <Link to={`/tacos/${taco}`}>{taco}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const Tacos = () => {
+  // Obtenemos las keys del segmento dinámico del path. Depende de cómo hayamos
+  // declarado nuestra ruta.
+  const { taco } = useParams()
+
+  return (
+    <div>
+      <h1>Tacos</h1>
+      <h2>{taco}</h2>
+    </div>
+  )
+}
+
+function App () {
   return (
     <div className="App">
       <header>
@@ -27,6 +65,7 @@ function App() {
         {/* No hay que enviar el componente, sino el elemento. Si pasamos solo el componente como {Home}, no funcionará correctamente. */}
         <Route path="/" element={<Home />} />
         <Route path="/search-page" element={<SearchPage />} />
+        <Route path="/tacos/:taco" element={<Tacos />} />
       </Routes>
     </div>
   )
