fc8491ca284e94a54fc2dcd5d903ae73e8d650fc
yeicobF <jacobrdz34@gmail.com>
2022-10-12 20:01:36 -0500

    Add: Iniciar curso React Router 6 de Midudev
    Iniciamos la configuraciÃn basica del proyecto y el linter con standard.
    En el video da un fallo App porque dice no encontrarlo y al parecer es
    porque no tenia instalado el plugin de eslint para el linter de React.
    Una vez instalado, ya no hay este problema.

---
+++ frontend/midudev-react-router-6/miduchollos/.eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["eslint:recommended", "standard", "plugin:react/recommended"],
+  rules: {
+    quotes: ["warn", "double"],
+    "comma-dangle": ["warn", "always-multiline"],
+  },
+}
