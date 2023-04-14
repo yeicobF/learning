c9ff7ffc6517939687c6078b2923257ee0641cce
yeicobF <jacobrdz34@gmail.com>
2023-04-14 01:22:32 -0600

    Feat: Reset original statem

--- pruebas-tecnicas/midu-prueba-tecnica-react-empresa-55k/src/App.tsx
+++ pruebas-tecnicas/midu-prueba-tecnica-react-empresa-55k/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { type User } from './types.d'
 import { UsersList } from './components/UsersList'
@@ -10,6 +10,16 @@ function App () {
   const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState<boolean>(false)
   const [sortByCountry, setSortByCountry] = useState<boolean>(false)
+  /** `useRef` -> Guardar un valor que se comparta entre renders, pero que al
+   * cambiar no vuelva a renderizar el componente. Si cambia el valor, no se
+   * vuelven a renderizar los componentes.
+   * - Para acceder al valor de una referencia y cambiarla, lo hacemos con
+   *   `current`.
+   *
+   * ! Es innecesario tener otro estado (`useState`), porque no se va a volver a
+   * actualizar o renderizar el valor.
+   */
+  const originalUsers = useRef<User[]>([])
 
   console.log({ showColors })
 
@@ -29,11 +39,16 @@ function App () {
     setUsers(fiteredUsers)
   }
 
+  const resetUsers = () => {
+    setUsers(originalUsers.current)
+  }
+
   useEffect(() => {
     fetch(`${USERS_API_URL}?results=${RESULTS_NUMBER}`)
       .then(async (res) => await res.json())
-      .then((res) => {
-        setUsers(res.results)
+      .then(({ results }) => {
+        setUsers(results)
+        originalUsers.current = results
       })
       .catch((err) => {
         console.log(err)
@@ -41,12 +56,12 @@ function App () {
   }, [])
 
   const sortedUsers = sortByCountry
-    // `sort` muta el array original, por lo que al volver a ordenar como
-    // originalmente estaba, seguiría ordenado. `toSorted` no está disponible en
-    // todos los navegadores, pero Babel/SWC lo transforman. Este método no muta
-    // el arreglo original.
-    // - Otra opción: [...users].sort((a, b) => { ... })
-    ? users.toSorted((a, b) => {
+    ? // `sort` muta el array original, por lo que al volver a ordenar como
+  // originalmente estaba, seguiría ordenado. `toSorted` no está disponible en
+  // todos los navegadores, pero Babel/SWC lo transforman. Este método no muta
+  // el arreglo original.
+  // - Otra opción: [...users].sort((a, b) => { ... })
+    users.toSorted((a, b) => {
       // Comparar strings dependiendo del idioma del usuario.
       // Si lo devolvemos directamente, lo hará de forma ascendente.
       return a.location.country.localeCompare(b.location.country)
@@ -61,8 +76,13 @@ function App () {
         <button onClick={toggleSortByCountry}>
           {sortByCountry ? 'No ordenar por país' : 'Ordenar por país'}
         </button>
+        <button onClick={resetUsers}>Reiniciar estado</button>
       </header>
-      <UsersList showColors={showColors} users={sortedUsers} deleteUser={handleDelete} />
+      <UsersList
+        showColors={showColors}
+        users={sortedUsers}
+        deleteUser={handleDelete}
+      />
     </div>
   )
 }
