35655f2cf990d36bcbc6eee9b8672f4df1a6ea22
yeicobF <jacobrdz34@gmail.com>
2023-04-24 03:43:33 -0600

    Feat(twemoji): Get posts with author

--- fullstack/twitter-with-only-emojis-theo-t3-stack-tutorial/src/server/api/routers/posts.ts
+++ fullstack/twitter-with-only-emojis-theo-t3-stack-tutorial/src/server/api/routers/posts.ts
@@ -1,12 +1,51 @@
+import type { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
 
 export const postsRouter = createTRPCRouter({
   // Un procedure es un método para generar una función que se llama desde el
   // cliente. Al ser público, indicamos que cualquier usuario puede acceder a
   // los posts.
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    console.log({ users });
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+      }
+      return {
+        post,
+        // Con el signo de exclamación le decimos a TypeScript que estamos seguros
+        // de que obtendremos la información.
+        author,
+      };
+    });
   }),
 });
