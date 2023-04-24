b0923606baecd240dcd99ea65938bd990342f3b9
yeicobF <jacobrdz34@gmail.com>
2023-04-24 04:04:22 -0600

    Feat: Render images with Image component

--- fullstack/twitter-with-only-emojis-theo-t3-stack-tutorial/src/pages/index.tsx
+++ fullstack/twitter-with-only-emojis-theo-t3-stack-tutorial/src/pages/index.tsx
@@ -6,6 +6,7 @@ import { type RouterOutputs, api } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
 
 dayjs.extend(relativeTime);
 
@@ -18,10 +19,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.profileImageUrl}
         alt="Profile image"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis!"
@@ -39,7 +42,13 @@ const PostView = (props: PostWithUser) => {
 
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img src={author.profileImageUrl} className="h-14 w-14 rounded-full" />
+      <Image
+        alt={`@${author.username}'s profile picture`}
+        src={author.profileImageUrl}
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <span> {`@${author.username}`}</span>
