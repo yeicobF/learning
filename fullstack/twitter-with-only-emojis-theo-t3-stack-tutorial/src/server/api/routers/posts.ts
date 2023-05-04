34e1633fb5f08dd737f3127bcbbb9ba9e91eaea6
yeicobF <jacobrdz34@gmail.com>
2023-05-04 03:08:25 -0600

    Feat(twemoji): Add create post method

--- fullstack/twitter-with-only-emojis-theo-t3-stack-tutorial/src/server/api/routers/posts.ts
+++ fullstack/twitter-with-only-emojis-theo-t3-stack-tutorial/src/server/api/routers/posts.ts
@@ -3,7 +3,11 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -20,6 +24,7 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: [{ createdAt: "desc" }],
     });
 
     const users = (
@@ -52,4 +57,25 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+
+  create: privateProcedure
+    .input(
+      // Con zod, validamos que el contenido del post sea un string con emojis.
+      // En caso de que no exista, TRPC devolverá un error.
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
