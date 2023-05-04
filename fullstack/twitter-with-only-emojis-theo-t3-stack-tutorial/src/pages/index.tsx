cd48274df94b57aa4379f104ee46a9d591fd267c
yeicobF <jacobrdz34@gmail.com>
2023-05-04 04:01:15 -0600

    Feat(twemoji): Add links to new pages

--- fullstack/twitter-with-only-emojis-theo-t3-stack-tutorial/src/pages/index.tsx
+++ fullstack/twitter-with-only-emojis-theo-t3-stack-tutorial/src/pages/index.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -67,7 +68,6 @@ const CreatePostWizard = () => {
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
         onKeyDown={(e) => {
-          
           if (e.key !== "Enter") return;
           if (input === "") return;
           e.preventDefault();
@@ -107,10 +107,15 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span> {`@${author.username}`}</span>
-          <span className="font-thin">{`· ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span> {`@${author.username}`}</span>
+          </Link>
+
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{`· ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-xl">{post.content}</span>
       </div>
