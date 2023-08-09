0736800fbb9c3126685f2021c8c4a6477527dc8c
yeicobF <jacobrdz34@gmail.com>
2023-08-09 02:53:45 -0600

    (vue): add not found page path redirect

--- fernando-herrera/vue.js-3-intermedio/composition-app/src/router/index.ts
+++ fernando-herrera/vue.js-3-intermedio/composition-app/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/shared/views/HomeView.vue'
+import AboutView from '@/shared/views/AboutView.vue'
+
+const REDIRECT_PATH_PATTERN = '/:pathMatch(.*)*'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,17 +13,21 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/counter',
-      name: 'counter',
-      component: () => import('@/counter/views/CounterView.vue')
-    },
-    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('@/shared/views/AboutView.vue')
+      component: AboutView
+    },
+    {
+      path: '/counter',
+      name: 'counter',
+      component: () => import('@/counter/views/CounterView.vue')
+    },
+    {
+      path: REDIRECT_PATH_PATTERN,
+      redirect: () => ({ name: 'home' })
     }
   ]
 })
