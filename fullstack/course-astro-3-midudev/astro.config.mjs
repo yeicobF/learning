663562f712fffbef576ce30ad82276d3bb446ad2
yeicob <jacobrdz34@gmail.com>
2023-10-01 03:31:44 -0600

    feat(astro-3-midu): add view transitions and hybrid generation

--- fullstack/course-astro-3-midudev/astro.config.mjs
+++ fullstack/course-astro-3-midudev/astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from "@astrojs/tailwind"
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   // Activar Server Side Rendering (SSR). Podemos tener varios output. Por
   // ejemplo, para cuando queremos tener partes estáticas y dinámicas. Podemos
   // indicarlo por cada una de las páginas. Por página, podemos utilizar `const
@@ -12,5 +13,5 @@ export default defineConfig({
   // un servidor para funcionar.
   //
   // Puedes utilizar diferentes adaptadores, tales como Vercel o Netlify.
-  output: "hybrid",
-})
+  output: "hybrid"
+});
