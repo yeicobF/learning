1f21dd0ca266a6a905426164a89bd3045f6c0412
yeicob <jacobrdz34@gmail.com>
2023-10-01 01:43:47 -0600

    feat(astro-3-midu): add specific launch page
    Use static rendering by fetching all available routes with
    `getStaticPaths`

--- fullstack/course-astro-3-midudev/astro.config.mjs
+++ fullstack/course-astro-3-midudev/astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+  // Activar Server Side Rendering (SSR). Podemos tener varios output. Por
+  // ejemplo, para cuando queremos tener partes estáticas y dinámicas. Podemos
+  // indicarlo por cada una de las páginas. Por página, podemos utilizar `const
+  // prerender = false` para que la página funcione en el servidor. Necesitará
+  // un servidor para funcionar.
+  //
+  // Puedes utilizar diferentes adaptadores, tales como Vercel o Netlify.
+  output: "hybrid",
+})
---
+++ fullstack/course-astro-3-midudev/src/services/spacex.ts
@@ -0,0 +1,37 @@
+import { type Doc, type APISpaceXResponse } from "../types/api"
+
+export const getLaunchById = async ({ id }: { id: string }) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
+  const launch = (await res.json()) as Doc
+
+  console.log(launch)
+
+  return launch
+}
+
+export const getLatestLaunches = async () => {
+  // Podemos ejecutar cualquier JavaScript que queramos.
+  // Podemos hacer un fetching de datos.
+
+  // Filtramos los datos que queremos obtener utilizando la ruta /query y haciendo
+  // un POST con los parámetros.
+
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "asc",
+        },
+        limit: 12,
+      },
+    }),
+  })
+  const { docs: launches } = (await res.json()) as APISpaceXResponse
+
+  return launches
+}
